e51d2375d4aa02c7ea78c8e2b062f299fb9085b5
Zach Taylor <zach@ztaylor.me>
2019-10-03 16:23:19 -0500

    change benchmark comparison order

--- bytes_bench_test.go
+++ bytes_bench_test.go
@@ -6,13 +6,6 @@ import (
 	"ztaylor.me/cast"
 )
 
-func BenchmarkStringBytes(b *testing.B) {
-	x := []byte{'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'}
-	for i := 0; i < b.N; i++ {
-		_ = cast.StringBytes(x)
-	}
-}
-
 func BenchmarkDefaultStringBytes(b *testing.B) {
 	x := []byte{'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'}
 	for i := 0; i < b.N; i++ {
@@ -20,10 +13,10 @@ func BenchmarkDefaultStringBytes(b *testing.B) {
 	}
 }
 
-func BenchmarkBytesS(b *testing.B) {
-	x := "hello world"
+func BenchmarkCastStringBytes(b *testing.B) {
+	x := []byte{'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'}
 	for i := 0; i < b.N; i++ {
-		_ = cast.BytesS(x)
+		_ = cast.StringBytes(x)
 	}
 }
 
@@ -31,5 +24,12 @@ func BenchmarkDefaultBytesS(b *testing.B) {
 	x := "hello world"
 	for i := 0; i < b.N; i++ {
 		_ = []byte(x)
+	}
+}
+
+func BenchmarkCastBytesS(b *testing.B) {
+	x := "hello world"
+	for i := 0; i < b.N; i++ {
+		_ = cast.BytesS(x)
 	}
 }
