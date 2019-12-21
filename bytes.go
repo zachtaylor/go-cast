06c6aa6cf604b17ee840209f352b82a9fe8382d3
Zach Taylor <zach@ztaylor.me>
2019-12-21 17:22:01 -0600

    separate unsafe.go

--- bytes.go
+++ bytes.go
@@ -1,17 +1,15 @@
 package cast
 
-import "unsafe"
-
 // StringBytes casts []byte to string
 //
 // outperforms `string(b)` by ~95%
 func StringBytes(b []byte) string {
-	return *(*string)(unsafe.Pointer(&b))
+	return *(*string)(Pointer(&b))
 }
 
 // BytesS casts string to []byte
 //
 // outperforms `[]byte(s)` by ~95%
 func BytesS(s string) []byte {
-	return *(*[]byte)(unsafe.Pointer(&s))
+	return *(*[]byte)(Pointer(&s))
 }
