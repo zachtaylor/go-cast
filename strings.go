0968ab6d55330795940b30eafd88b49fcb348ae4
Zach Taylor <zach@ztaylor.me>
2019-12-21 17:17:20 -0600

    add strings.Contains, LastIndex, Trim

--- strings.go
+++ strings.go
@@ -5,14 +5,22 @@ import "strings"
 // StringBuilder is an alias for strings.Builder
 type StringBuilder = strings.Builder
 
-// // ReplaceAll is an alias for strings.ReplaceAll
-// func ReplaceAll(s, old, new string) string {
-// 	return strings.ReplaceAll(s, old, new)
-// }
-
 var replacer = strings.NewReplacer(`\`, `\\`, `"`, `\"`)
 
+// Contains uses strings.Contains
+func Contains(a, b string) bool { return strings.Contains(a, b) }
+
+// InCharset returns len(Trim()) < 1
+func InCharset(a, b string) bool { return len(Trim(a, b)) < 1 }
+
 // EscapeString escapes '\' and '"' characters
-func EscapeString(s string) string {
-	return replacer.Replace(s)
-}
+func EscapeString(s string) string { return replacer.Replace(s) }
+
+// LastIndex is an alias for strings.LastIndex
+func LastIndex(a, b string) int { return strings.LastIndex(a, b) }
+
+// Trim uses strings.Trim
+func Trim(a, b string) string { return strings.Trim(a, b) }
+
+// OutCharset returns len(Trim()) > 0
+func OutCharset(a, b string) bool { return len(Trim(a, b)) > 0 }
