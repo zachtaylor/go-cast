8c349450286e8e6fbcf68416b0079b3a52959c1b
Zach Taylor <zach@ztaylor.me>
2019-12-21 18:04:51 -0600

    add bytes.Buffer

--- bytes.go
+++ bytes.go
@@ -1,5 +1,15 @@
 package cast
 
+import "bytes"
+
+// Buffer = bytes.Buffer
+type Buffer = bytes.Buffer
+
+// NewBuffer uses bytes.NewBufferString
+func NewBuffer(s string) *Buffer {
+	return bytes.NewBufferString(s)
+}
+
 // StringBytes casts []byte to string
 //
 // outperforms `string(b)` by ~95%
