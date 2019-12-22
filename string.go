7c86efb5b2ce934f1727e57f39c3849de79e2e82
Zach Taylor <zach@ztaylor.me>
2019-12-21 18:05:01 -0600

    add StringN

--- string.go
+++ string.go
@@ -1,29 +1,54 @@
 package cast
 
-// String cast any value to string
-func String(arg interface{}) string {
+// String casts any value to string
+func String(arg interface{}) (str string) {
 	switch v := arg.(type) {
 	case string:
-		return v
+		str = v
 	case IStringer:
-		return v.String()
+		str = v.String()
 	case []byte:
-		return StringBytes(v)
+		str = StringBytes(v)
 	case error:
-		return v.Error()
+		str = v.Error()
 	case bool:
-		return StringB(v)
+		str = StringB(v)
 	case int:
-		return StringI(v)
+		str = StringI(v)
 	case uint:
-		return StringUI(v)
+		str = StringUI(v)
 	case int64:
-		return StringI64(v)
+		str = StringI64(v)
 	case float64:
-		return StringF(v)
+		str = StringF(v)
 	default:
-		return Sprint(arg)
+		str = Sprint(arg)
 	}
+	return
+}
+
+// StringN cast any number of values to string
+func StringN(args ...interface{}) (str string) {
+	if args == nil || len(args) < 1 {
+		// skip
+	} else if len(args) == 1 {
+		str = String(args[0])
+	} else {
+		sb, first := poolStringBuilder.Get().(*StringBuilder), true
+		sb.Grow(32 * len(args))
+		for _, arg := range args {
+			if first {
+				first = false
+			} else {
+				sb.WriteByte(' ')
+			}
+			sb.WriteString(String(arg))
+		}
+		str = sb.String()
+		sb.Reset()
+		poolStringBuilder.Put(sb)
+	}
+	return
 }
 
 // StringB casts bool to string
