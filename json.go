6d89ba501914ca3a4a1762fa16622a281c9eddb9
Zach Taylor <zach@ztaylor.me>
2019-12-26 14:52:57 -0600

    move DecodeJSON to json.go

--- json.go
+++ json.go
@@ -59,3 +59,8 @@ func (json JSON) Copy() (j JSON) {
 	}
 	return
 }
+
+// DecodeJSON wraps encoding/json.Decoder.Decode
+func DecodeJSON(r Reader, v interface{}) error {
+	return json.NewDecoder(r).Decode(v)
+}
