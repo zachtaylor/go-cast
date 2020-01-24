7fb8e9bf438900179306d919d0972f126ea21c0e
Zach Taylor <zach@ztaylor.me>
2020-01-24 10:49:30 -0600

    add weight to benchmark map data 1

--- cast_test.go
+++ cast_test.go
@@ -15,19 +15,21 @@ func MakeDataSlice() []interface{} {
 
 func MakeDataMap1() map[string]interface{} {
 	return map[string]interface{}{
-		"k1":   "v1",
-		"k2":   "v2",
-		"k3":   "v3",
-		"i1":   1,
-		"i2":   2,
-		"i3":   3,
-		"b1":   true,
-		"b2":   false,
-		"map":  map[string]interface{}{},
-		"json": cast.JSON{},
-		"dat1": "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-		"dat2": "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-		"dat3": "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz",
+		"k1":    "v1",
+		"k2":    "v2",
+		"k3":    "v3",
+		"i1":    1,
+		"i2":    2,
+		"i3":    3,
+		"b1":    true,
+		"b2":    false,
+		"map":   map[string]interface{}{},
+		"json":  cast.JSON{},
+		"slice": []interface{}{"hello", "world"},
+		"array": cast.Array{"foo", "bar"},
+		"dat1":  "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+		"dat2":  "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+		"dat3":  "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz",
 	}
 }
 
