3668192558953b4c0cab22b15dffcafea4a88654
Zach Taylor <zach@ztaylor.me>
2020-01-24 10:45:58 -0600

    add docs

--- io.go
+++ io.go
@@ -31,14 +31,17 @@ type Closer = io.Closer
 // WriteCloser = io.WriteCloser
 type WriteCloser = io.WriteCloser
 
+// WriteBytes is an alias for Writer.Write
 func WriteBytes(w Writer, b []byte) (int, error) {
 	return w.Write(b)
 }
 
+// WriteString returns WriteBytes(cast []byte from string)
 func WriteString(w Writer, s string) (int, error) {
 	return WriteBytes(w, BytesS(s))
 }
 
+// WriteN is returns WriteString(StringN(...))
 func WriteN(w Writer, args ...interface{}) (int, error) {
 	return WriteString(w, StringN(args...))
 }
